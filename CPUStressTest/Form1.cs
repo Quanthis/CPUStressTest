f92a035d9423d10172ad566aea3f8c6fe4427421
Młyński Stanisław <mlynski.stanislaw@gmail.com>
2020-09-11 17:56:41 +0200

    Application is functional now --Added handling for UI --Application manages to use 70% of Ryzen 5 3600 already at test start (if proper amount of threads is given)

--- CPUStressTest/Form1.cs
+++ CPUStressTest/Form1.cs
@@ -22,9 +22,45 @@ namespace CPUStressTest
 
         }
 
-        private void START_TEST_Click(object sender, EventArgs e)
+        private async void START_TEST_Click(object sender, EventArgs e)
         {
+            try
+            {
+                ushort threadsNo = Convert.ToUInt16(TextBox_NumberOfThreads.Text);
 
+                var TEST = new PerformTest(threadsNo);
+
+                if(ChangeDisplayedValue(true) != true)
+                {
+                    MessageBox.Show("Internal Error!", "Please concact our onsite support!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+
+                bool operationSuccessfull = await TEST.Perform();
+
+                MessageBox.Show("Test finished! ", "Your PC has managed to finish test without turnig off!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch(InvalidCastException)
+            {
+                MessageBox.Show("User Error!", "Number of threads may only be in range 1-65535", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private bool ChangeDisplayedValue(bool increaseOrNot)
+        {
+            int value = Convert.ToInt32(Label_Running_Tests.Text);
+
+            if(increaseOrNot)
+            {
+                ++value;
+            }
+            else
+            {
+                --value;
+            }
+
+            Label_Running_Tests.Text = value.ToString();
+
+            return true;
         }
     }
 }
